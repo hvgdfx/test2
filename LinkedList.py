00357ddec05a1c30fccad95175e5a740b57d7d12
hvgdfx <zgx_007@qq.com>
2018-04-27 23:15:41 +0800

    add LinkedList.py

---
+++ LinkedList.py
@@ -0,0 +1,14 @@
+
+
+class Node(object):
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+'''
+
+L = Node('a', Node('b', Node('c', Node('d'))))
+
+
+'''
