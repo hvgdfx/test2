69d95f3edbc9ce2e11f0246edb4bc1dfbc802eaf
hvgdfx <zgx_007@qq.com>
2018-04-28 11:43:03 +0800

    update

--- linkedlist/LinkedList.py
+++ linkedlist/LinkedList.py
@@ -12,3 +12,17 @@ L = Node('a', Node('b', Node('c', Node('d'))))
 
 
 '''
+
+class SingleLinkedList:
+    def __init__(self, elem):
+        self.elem = elem
+        self.__head = None
+    def add(self,):
+        pass
+    def append(self, item):
+        pass
+    def length(self):
+        pass
+
+if __name__ == '__main__':
+    print('success')
