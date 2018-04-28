0c985f7dba6c5282908fee01f3084fdc7de0713d
hvgdfx <zgx_007@qq.com>
2018-04-28 20:53:15 +0800

    update

--- linkedlist/LinkedList.py
+++ linkedlist/LinkedList.py
@@ -1,8 +1,8 @@
 
 
 class Node(object):
-    def __init__(self, value, next=None):
-        self.value = value
+    def __init__(self, elem):
+        self.elem = elem
         self.next = next
 
 
@@ -13,16 +13,29 @@ L = Node('a', Node('b', Node('c', Node('d'))))
 
 '''
 
-class SingleLinkedList:
+class SingleLinkedList(object):
     def __init__(self, elem):
         self.elem = elem
-        self.__head = None
-    def add(self,):
-        pass
-    def append(self, item):
+        self.next = None
+    def is_empty(self):
         pass
     def length(self):
         pass
+    def travel(self):
+        pass
+    def add(self, pos, item):
+        pass
+    def append(self, item):
+        pass
+    def insert(self, pos, item):
+        pass
+    def remove(self, item):
+        pass
+    def search(self, item):
+        pass
+    
+
+
 
 if __name__ == '__main__':
     print('success')
