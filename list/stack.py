696e14ec0cc86a9debbd42ede7cc49572bfd5395
hvgdfx <zgx_007@qq.com>
2018-05-01 17:14:03 +0800

    update

--- list/stack.py
+++ list/stack.py
@@ -1,12 +1,28 @@
 
 class Stack(object):
+
     def __init__(self):
 	self.item = []
 
-    def push(self, item):
-	pass
+    def push(self, elem):
+	self.item.append(elem)
+	return self.item
 
     def pop(self):
-	pass
+	self.item.pop()
+	return self.item
 
+    def peek(self):
+	if self.is_empty():
+	    print("This stack is empty")
+        else:
+	    return self.item[len(self.item)-1]
 
+    def is_empty(self):
+	return len(self.item) == 0
+
+if __name__ == '__main__':
+    stack = Stack()
+    stack.is_empty()
+
+    stack.push(100)
