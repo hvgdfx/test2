45c70b4b69cdb248e78d8c6cd112f549d4021a53
hvgdfx <zgx_007@qq.com>
2018-05-01 18:51:50 +0800

    update

---
+++ 01_list/06_doublelist.py
@@ -0,0 +1,21 @@
+
+class Node(object):
+    def __init__(self, item)
+	self.elem = item
+	self.prev = None
+	self.next = None
+
+class DoubleLinkList(object):
+
+    def __init__(self, node=None):
+	self.__head = node
+
+    def is_empty(self):
+	return self.__head == None
+
+    def 
+
+
+if __name == '__main__':
+
+    dll = DoubleLinkList()
