9df5cf77352098522e319fea992ad7ae26ccf284
hvgdfx <zgx_007@qq.com>
2018-05-06 21:48:49 +0800

    update

--- 04_tree/tree.py
+++ 04_tree/tree.py
@@ -3,40 +3,92 @@
 class Node(object):
     def __init__(self, item):
 	self.elem = item
-	self.next = None
-
-class Tree(object):
-    def __init__(self):
-	self.tree = []
 	self.lchild = None
 	self.rchild = None
 
+class Tree(object):
+    def __init__(self):
+	self.root = None
+
     def add(self, item):
 	node = Node(item)
-	if self.lchild == None:
-	    self.lchild = node
-	if self.rchild == None:
-	    self.rchild = node	    	    
-	
+	if self.root == None:
+	    self.root = node
+	    return 
+	queue = [self.root]	
+	while queue:
+	    cur_node = queue.pop(0)
+	    if cur_node.lchild is None:
+                cur_node.lchild = node
+          	return
+	    else:
+	        queue.append(cur_node.lchild)
+            if cur_node.rchild is None:
+	        cur_node.rchild = node
+	        return
+            else:
+	        queue.append(cur_node.rchild)
+
+    def breadth_order(self):
+	queue = [self.root]
+
+	if self.root is None:
+	    return
+	while queue:	
+	    cur_node = queue.pop(0)
+	    print(cur_node.elem),
+	    if cur_node.lchild is not None:
+	        queue.append(cur_node.lchild)
+	    if cur_node.rchild is not None:
+	        queue.append(cur_node.rchild)
 
 
-    def breath_order(self):
-	pass
+    def pre_order(self, node):
+	if node is None:
+	    return
+	print(node.elem),
+	self.pre_order(node.lchild)
+	self.pre_order(node.rchild)
 
-    def pre_order(self):
-	pass
+    def in_order(self, node):
+	if node is None:
+	    return
+	self.in_order(node.lchild)
+	print(node.elem),
+	self.in_order(node.rchild)
 
-    def inorder(self):
-	pass
-
-    def post_order(self):
-	pass	
+    def post_order(self, node):
+	if node is None:
+	    return
+	self.post_order(node.lchild)
+	self.post_order(node.rchild)
+	print(node.elem),
 
 if __name__ == '__main__':
     tree = Tree()
     print('build a tree')
 
-    tree.add('a')
+
+    tree.add(0)
+    tree.add(1)
+    tree.add(2)
+    tree.add(3)
+    tree.add(4)
+    tree.add(5)
+    tree.add(6)
+    tree.add(7)
+    tree.add(8)
+    tree.add(9)
+
+
+    tree.breadth_order()
+    print('\n')
+    tree.pre_order(tree.root)
+    print('\n')
+    tree.in_order(tree.root)
+    print('\n')
+    tree.post_order(tree.root)
+
 
 
 
